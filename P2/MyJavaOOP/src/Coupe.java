18435040cb97e326898aa9167449df9c1becb74b
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2024-05-25 15:16:25 +0200

    Delete P2/MyJavaOOP directory

--- P2/MyJavaOOP/src/Coupe.java
+++
@@ -1,5 +0,0 @@
-public class Coupe extends Car {
-    public Coupe(String make, String model, int miles, int price) {
-        super(make, model, miles, price, 2);
-    }
-}
--- P2/MyJavaOOP/src/Sedan.java
+++
@@ -1,5 +0,0 @@
-public class Sedan extends Car {
-    public Sedan(String make, String model, int miles, int price) {
-        super(make, model, miles, price, 4);
-    }
-}

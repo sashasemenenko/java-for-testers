f663cf36644d9cc443aec2de669e03f070215988
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2024-05-28 11:36:11 +0200

    Add files via upload

--- P4/MyJunitTest/src/main/java/CalculatorDemo.java
+++ P4/MyJunitTest/src/main/java/CalculatorDemo.java
@@ -1,1 +1,9 @@
+public class CalculatorDemo {
+    public double add(double a, double b) {
+        return a + b;
+    }
 
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+}
